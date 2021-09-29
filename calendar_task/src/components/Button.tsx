2ca2eb42716def371aa46a531b9d313a892ea3e5
b <b>
2021-09-29 15:46:49 +0100

    Added: Reusable button component with tests

---
+++ calendar_task/src/components/Button.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@material-ui/core";
+import { MouseEventHandler } from "react";
+
+interface Button {
+  onClick: MouseEventHandler | any;
+  children: React.ReactNode;
+}
+
+const Buttons = ({ onClick, children }: Button) => (
+  <button onClick={(e) => onClick(e)}>{children}</button>
+);
+
+export default Buttons;
